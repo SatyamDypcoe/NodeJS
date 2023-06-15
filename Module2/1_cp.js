83cf2eb7d6d69ad362c07cf9c9c6a6b282bb32a2
Satyam <tsingh30399@gmail.com>
2023-06-16 04:03:43 +0530

    Child process

---
+++ Module2/1_cp.js
@@ -0,0 +1,8 @@
+// Child process is a node module used to create sub process within a script
+// you can perform different task with your script by just using some method
+
+const cp = require('child_process')
+
+
+// cp.execSync('calc')
+cp.execSync('start chrome')
