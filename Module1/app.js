5b9e12fa46b4b63d21d63cc931f5f8cb2fe2d0b5
Satyam <tsingh30399@gmail.com>
2023-06-16 03:33:12 +0530

    first commit

---
+++ Module1/app.js
@@ -0,0 +1,9 @@
+console.log('Welecome to NodeJs')
+
+function hello(){
+    console.log("Hello from Satyam")
+}
+hello()
+// To run in terminal open integrated terminal and type "node fileName"
+
+console.log(global)
---
+++ Module2/app.js
@@ -0,0 +1,8 @@
+const calculator = require('./calculator')
+
+
+calculator.addition(3,4)
+calculator.subtraction(8,2)
+calculator.multiplication(3,6)
+calculator.division(9,3)
+
